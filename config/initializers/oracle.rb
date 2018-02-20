0b0731ea79be38746c37a9e9e511395cef04cbc0
Dirk Kelly <github@dirkkelly.com>
2018-02-20 11:33:17 -0700

    removed everything

--- config/initializers/oracle.rb
+++
@@ -1,5 +0,0 @@
-ActiveSupport.on_load(:active_record) do
-  ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.class_eval do
-    
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsOracle::Application.config.secret_token = '01cec0c890532a242be3c5066ceb136fb86f7a36c3a1381327dd52c51f28bc317b0e74bbeac63c92271014c0f49e953d1176696409e906d27aaae2d624eea788'
